0d75f38f80630527e3b903be79f4dd01609adb6f
master <kittitat.2012@gmail.com>
2016-03-12 15:22:52 +0700

    Initial commit

---
+++ app/src/main/java/com/finalproject/cooperativeeducation/SplashScreenActivity.java
@@ -0,0 +1,48 @@
+package com.finalproject.cooperativeeducation;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Handler handler;
+    Runnable runnable;
+    long delay_time;
+    long time = 3000l;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+        handler = new Handler();
+        runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                // next activity
+            }
+        };
+        //test commit git
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        delay_time = time;
+        handler.postDelayed(runnable, delay_time);
+        time = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        handler.removeCallbacks(runnable);
+        time = delay_time - (System.currentTimeMillis() - time);
+    }
+}
